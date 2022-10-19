4897920f0073d5e2353971c8aa41279655dc6c02
Widura Schwittek <widura.schwittek@it-objects.de>
2022-10-19 22:35:10 +0200

    chore: adding description to module output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "cloudfront_domain_name" {
-  value = module.environment.cloudfront_default_domain_name
+  value       = module.environment.cloudfront_default_domain_name
+  description = "URL of Cloudfront distribution. Please wait some minutes until the distribution becomes available."
 }
