803b5f43888ca2238000fc247a5a6d241a56dadd
Dennis Eicker <dennis.eicker@it-objects.de>
2022-10-19 16:50:53 +0200

    feat(gh-pages): Added basic gh-pages using vitepress
    re [TERRA3-37]

---
+++ gh-pages/docs/.vitepress/config.js
@@ -0,0 +1,49 @@
+export default {
+    title: 'Terra3',
+    //base: '/terra3-website/',
+    description: 'Documentation for Terra3.',
+    lastUpdated: true,
+    lang: 'en-US',
+    themeConfig: {
+        // SEE: https://vitepress.vuejs.org/guide/theme-nav AND https://github.com/vuejs/vitepress/blob/main/docs/.vitepress/config.ts
+        //logo: '/my-logo.svg',
+        //siteTitle: false
+
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/it-objects/terraform-aws-terra3' }
+        ],
+
+        editLink: {
+            pattern: 'https://github.com/it-objects/terraform-aws-terra3/edit/main/gh-pages/docs/:path',
+            text: 'Edit this page on GitHub'
+        },
+
+        footer: {
+            message: 'Released under the Apache2 License.',
+            copyright: 'Copyright © 2022 it-objects GmbH'
+        },
+
+        nav: [
+          { text: 'IT-Objects', link: 'https://www.it-objects.de/cloud/' },
+          /*{
+            text: 'Dropdown Menu',
+            collapsible: true,
+            items: [
+              { text: 'Item A', link: '/item-1' },
+              { text: 'Item B', link: '/item-2' },
+              { text: 'Item C', link: '/item-3' }
+            ]
+          }*/
+        ],
+        sidebar: [
+            {
+              text: 'Guide',
+              collapsible: true,
+              items: [
+                { text: 'Introduction', link: '/introduction' },
+                { text: 'Getting Started', link: '/getting-started' },
+              ]
+            }
+          ]
+      }
+  }
