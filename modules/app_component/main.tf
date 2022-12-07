d9728a82a8e8ab1239c11af52619ad7dfb156b66
kaushik.katariya <kaushik.katariya@it-objects.de>
2022-12-07 14:19:32 +0100

    feat: added resources of ecs_service_autoscaling

--- modules/app_component/main.tf
+++ modules/app_component/main.tf
@@ -68,6 +68,82 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
+# Autoscaling based on CPU and MEMORY Utilisation for scaling up and down to save costs.
+# ---------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
+# Create autoscaling target linked to ECS
+# ---------------------------------------------------------------------------------------------------------------------
+resource "aws_appautoscaling_target" "ecs_target" {
+  max_capacity       = 5
+  min_capacity       = 1
+  resource_id        = "service/${var.container_runtime}/${aws_ecs_service.ecs_service.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+  role_arn           = aws_iam_role.ecs-autoscale-role.arn
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Scale up based on CPU and MEMORY Utilisation
+# ---------------------------------------------------------------------------------------------------------------------
+resource "aws_appautoscaling_policy" "ecs_target_cpu" {
+  name               = "application-scaling-policy-cpu"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+    target_value = 70
+  }
+  depends_on = [aws_appautoscaling_target.ecs_target]
+}
+
+resource "aws_appautoscaling_policy" "ecs_target_memory" {
+  name               = "application-scaling-policy-memory"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+    target_value = 70
+  }
+  depends_on = [aws_appautoscaling_target.ecs_target]
+}
+# ---------------------------------------------------------------------------------------------------------------------
+# IAM Role Definitions
+# ---------------------------------------------------------------------------------------------------------------------
+resource "aws_iam_role" "ecs-autoscale-role" {
+  name = "ecs-scale-application-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "application-autoscaling.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-autoscale" {
+  role       = aws_iam_role.ecs-autoscale-role.id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceAutoscaleRole"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
 # Link to loadbalancer: target group and lb listener
 # ---------------------------------------------------------------------------------------------------------------------
 resource "aws_lb_target_group" "target_group" {
