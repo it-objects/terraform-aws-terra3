3cf9da43c24be536ec36ffcc152a3786114225bf
kaushik.katariya <kaushik.katariya@it-objects.de>
2022-12-13 11:24:22 +0100

    feat: added application load balancer logs in S3 bucket.

--- examples/ALB_logs/outputs.tf
+++ examples/ALB_logs/outputs.tf
@@ -1,3 +1,5 @@
+
+
 output "cloudfront_default_domain_name" {
   value       = module.terra3_examples.cloudfront_domain_name
   description = "URL of Cloudfront distribution. Please wait some minutes until the distribution becomes available."
@@ -5,5 +7,10 @@ output "cloudfront_default_domain_name" {
 
 output "static_website_url" {
   value       = "https://${module.terra3_examples.cloudfront_domain_name}/"
+  description = "URL of Cloudfront distribution. Please wait some minutes until the distribution becomes available."
+}
+
+output "container_url" {
+  value       = "https://${module.terra3_examples.cloudfront_domain_name}/api/"
   description = "URL of Cloudfront distribution. Please wait some minutes until the distribution becomes available."
 }
