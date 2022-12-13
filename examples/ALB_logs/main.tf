54161776b0ad11b650a328d104492984f72d46c2
kaushik.katariya <kaushik.katariya@it-objects.de>
2022-12-13 10:04:04 +0100

    wip: initial commit for alb logs

---
+++ examples/ALB_logs/main.tf
@@ -0,0 +1,22 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# This is example 1 showcasing Terra3's capabilities.
+#
+# Outcome: Environment with S3 bucket serving a static website via Cloudfront and a static Cloudfront URL
+# Remarks: This is the most minimal solution which runs without further effort out-of-the-box in an empty
+#          AWS account.
+# ---------------------------------------------------------------------------------------------------------------------
+
+locals {
+  solution_name = "alb-example"
+}
+
+module "terra3_examples" {
+  source = "../.."
+
+  solution_name                 = local.solution_name
+  enable_account_best_practices = true
+
+  # configure your environment here
+  create_load_balancer = true
+  nat                  = "NO_NAT" # default value
+}
--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -2,6 +2,7 @@
 # Application Loadbalancer
 # ---------------------------------------------------------------------------------------------------------------------
 #tfsec:ignore:aws-elb-alb-not-public
+#tfsec:ignore:aws-s3-enable-versioning
 resource "aws_lb" "this" {
   name               = "${var.solution_name}-alb"
   internal           = false
@@ -11,6 +12,72 @@ resource "aws_lb" "this" {
 
   enable_deletion_protection = false
   drop_invalid_header_fields = true
+
+  access_logs {
+    bucket  = aws_s3_bucket.lb-logs.bucket
+    prefix  = "${var.solution_name}-alb"
+    enabled = true
+  }
+}
+
+#tfsec:ignore:aws-s3-specify-public-access-block
+#tfsec:ignore:aws-s3-enable-versioning
+#tfsec:ignore:aws-s3-enable-bucket-logging
+#tfsec:ignore:aws-s3-encryption-customer-key
+#tfsec:ignore:aws-s3-no-public-buckets
+#tfsec:ignore:aws-s3-ignore-public-acls
+#tfsec:ignore:aws-s3-enable-bucket-encryption
+#tfsec:ignore:aws-s3-block-public-policy
+#tfsec:ignore:aws-s3-block-public-acls
+resource "aws_s3_bucket" "lb-logs" {
+  bucket = "${var.solution_name}-alb-logs-s3-bucket-${random_string.random_s3_alb_logs_postfix.result}"
+}
+
+resource "aws_s3_bucket_acl" "lb-logs-acl" {
+  bucket = aws_s3_bucket.lb-logs.id
+  acl    = "private"
+}
+
+data "aws_iam_policy_document" "allow-lb" {
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["logdelivery.elb.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:PutObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.lb-logs.arn}/*"
+    ]
+  }
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["054676820928"]
+    }
+
+    actions = [
+      "s3:PutObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.lb-logs.arn}/*"
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "allow-lb" {
+  bucket = aws_s3_bucket.lb-logs.id
+  policy = data.aws_iam_policy_document.allow-lb.json
+}
+
+resource "random_string" "random_s3_alb_logs_postfix" {
+  length    = 4
+  special   = false
+  min_lower = 4
 }
 
 resource "aws_ssm_parameter" "environment_alb_arn" {
