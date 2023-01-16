24ee732256b0ebac17e8e43bd8ccd4e11352ba9d
kaushik.katariya <kaushik.katariya@it-objects.de>
2023-01-16 14:53:42 +0100

    wip: added default values and switch in new eventbridge module.

--- modules/scheduled_api_call/variables.tf
+++ modules/scheduled_api_call/variables.tf
@@ -1,3 +1,9 @@
+variable "enable_eventbridge_scheduled_Api_call" {
+  type        = bool
+  description = "Select true to enable eventbridge scheduled api call."
+  default     = false
+}
+
 variable "solution_name" {
   type = string
 }
@@ -17,14 +23,35 @@ variable "scheduled_api_call_http_method" {
 variable "scheduled_api_call_url" {
   type        = string
   description = "URL to be triggered."
+  default     = ""
 }
 
 variable "scheduled_api_apikey_key" {
   type        = string
   description = "apikey key as query param."
+  default     = ""
 }
 
 variable "scheduled_api_apikey_value" {
   type        = string
   description = "apikey value as query param."
+  default     = ""
+}
+
+variable "enable_scheduled_https_api_call" {
+  type        = bool
+  description = "Select true to enable scheduled api call."
+  default     = false
+}
+
+variable "scheduled_https_api_call_crontab" {
+  type        = string
+  description = "Enter schedule details of scheduled api call in crontab format."
+  default     = ""
+}
+
+variable "scheduled_https_api_call_url" {
+  type        = string
+  description = "Enter url of scheduled api call."
+  default     = ""
 }
