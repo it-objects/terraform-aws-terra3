b7ef9b9e756c77b908586964a995c94b89848071
kaushik Katariya <76080457+Kaushikkatariya@users.noreply.github.com>
2023-01-17 13:50:52 +0100

    feat: adding scheduled api calls for e.g. maintenance activities (currently limited to unauthorized GET requests)
    Co-authored-by: kaushik.katariya <kaushik.katariya@it-objects.de>
    Co-authored-by: Widura Schwittek <widura.schwittek@it-objects.de>

--- variables.tf
+++ variables.tf
@@ -373,3 +373,21 @@ variable "ses_mail_from_domain" {
   type        = string
   default     = ""
 }
+
+variable "enable_scheduled_https_api_call" {
+  type        = bool
+  description = "Select true to enable scheduled api call."
+  default     = false
+}
+
+variable "scheduled_https_api_call_crontab" {
+  type        = string
+  description = "Enter schedule details of scheduled api call in crontab format."
+  default     = ""
+}
+
+variable "scheduled_https_api_call_url" {
+  type        = string
+  description = "Enter url of scheduled api call."
+  default     = ""
+}
