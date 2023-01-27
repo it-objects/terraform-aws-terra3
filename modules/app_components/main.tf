74bf9d609dfd3ab9e294fd5c33a912cc84f22dce
kaushik Katariya <76080457+Kaushikkatariya@users.noreply.github.com>
2023-01-27 15:49:00 +0100

    fix: added default value of firelens container (#26)
    Co-authored-by: kaushik.katariya <kaushik.katariya@it-objects.de>

--- modules/app_components/main.tf
+++ modules/app_components/main.tf
@@ -38,7 +38,7 @@ module "app_components" {
 
   container = each.value["container"]
 
-  enable_firelens_container = each.value["enable_firelens_container"]
+  enable_firelens_container = lookup(each.value, "enable_firelens_container", false)
 
   execution_iam_access = lookup(each.value, "execution_iam_access", null)
 
