3a71814f56d67a87b08aedf4884db63388b9deba
Widura Schwittek <widura.schwittek@it-objects.de>
2023-04-27 16:27:10 +0200

    feat: add alias domain (#32)
    Alias domains current usage is to allow besides the auto-generated internal domain to additionally attach an external custom domain

--- modules/dns_and_certificates/outputs.tf
+++ modules/dns_and_certificates/outputs.tf
@@ -10,6 +10,10 @@ output "domain_name" {
   value = aws_acm_certificate.domain_certificate.domain_name
 }
 
+output "internal_domain_name" {
+  value = length(var.alias_domain_name) == 0 ? aws_acm_certificate.domain_certificate.domain_name : tolist(aws_acm_certificate.domain_certificate.subject_alternative_names)[0]
+}
+
 output "subject_alternative_names" {
   value = aws_acm_certificate.domain_certificate.subject_alternative_names
 }
