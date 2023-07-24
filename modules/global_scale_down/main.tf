208790d80f665167405f227babbee2afd019afaa
kaushik.katariya <kaushik.katariya@it-objects.de>
2023-07-24 16:25:56 +0200

    feat: added s3 bucket, function url and secret.

--- modules/global_scale_down/main.tf
+++ modules/global_scale_down/main.tf
@@ -22,9 +22,10 @@ locals {
     aws_iam_policy.scale_down_redis_policy[*].arn
   )
 
-  ecs_service_data    = "/${var.solution_name}/global_scale_down/ecs_service_data"
-  scale_up_parameters = "/${var.solution_name}/global_scale_down/scale_up_parameters"
-  hibernation_state   = " /${var.solution_name}/global_scale_down/hibernation_state"
+  ecs_service_data         = "/${var.solution_name}/global_scale_down/ecs_service_data"
+  scale_up_parameters      = "/${var.solution_name}/global_scale_down/scale_up_parameters"
+  hibernation_state        = " /${var.solution_name}/global_scale_down/hibernation_state"
+  admin_secret_credentials = aws_secretsmanager_secret.hashed_credentials.arn
 }
 
 module "lambda_scale_up" {
@@ -56,9 +57,10 @@ module "lambda_scale_up" {
   number_of_policies = length(local.scale_up_policies_arns)
 
   environment_variables = {
-    ecs_service_data    = local.ecs_service_data
-    scale_up_parameters = local.scale_up_parameters
-    hibernation_state   = local.hibernation_state
+    ecs_service_data         = local.ecs_service_data
+    scale_up_parameters      = local.scale_up_parameters
+    hibernation_state        = local.hibernation_state
+    admin_secret_credentials = local.admin_secret_credentials
   }
 }
 
@@ -129,9 +131,10 @@ module "lambda_scale_down" {
   number_of_policies = length(local.scale_down_policies_arns)
 
   environment_variables = {
-    ecs_service_data    = local.ecs_service_data
-    scale_up_parameters = local.scale_up_parameters
-    hibernation_state   = local.hibernation_state
+    ecs_service_data         = local.ecs_service_data
+    scale_up_parameters      = local.scale_up_parameters
+    hibernation_state        = local.hibernation_state
+    admin_secret_credentials = local.admin_secret_credentials
   }
 }
 
@@ -162,4 +165,109 @@ module "eventbridge_scale_down" {
       }
     ]
   }
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "${var.solution_name}-global-scale-up-and-down-hosting-bucket-${random_string.random_s3_postfix.result}"
+}
+
+resource "random_string" "random_s3_postfix" {
+  length    = 4
+  special   = false
+  min_lower = 4
+}
+
+resource "aws_s3_bucket_website_configuration" "b_website" {
+  bucket = aws_s3_bucket.bucket.id
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.bucket.id
+  key    = "index.html"
+  source = "${path.module}/index.html"
+}
+
+resource "aws_s3_bucket_ownership_controls" "s3_data_bucket" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_lambda_function_url" "scale_down_lambda_function_url" {
+  function_name      = module.lambda_scale_down[0].lambda_function_name
+  authorization_type = "NONE" #"AWS_IAM"
+
+  cors {
+    allow_credentials = false
+    allow_origins     = ["*"]
+    allow_methods     = ["GET", "POST"]
+    max_age           = 86400
+  }
+}
+
+resource "aws_lambda_function_url" "scale_up_lambda_function_url" {
+  function_name      = module.lambda_scale_up[0].lambda_function_name
+  authorization_type = "NONE" #"AWS_IAM"
+
+  cors {
+    allow_credentials = false
+    allow_origins     = ["*"]
+    allow_methods     = ["GET", "POST"]
+    max_age           = 86400
+  }
+}
+
+data "template_file" "index" {
+  template = file("${path.module}/index.tpl")
+
+  vars = {
+    scale_down_api_endpoint = aws_lambda_function_url.scale_down_lambda_function_url.function_url
+    scale_up_api_endpoint   = aws_lambda_function_url.scale_up_lambda_function_url.function_url
+  }
+}
+
+resource "local_file" "local_index" {
+  content  = data.template_file.index.rendered
+  filename = "${path.module}/index.html"
+}
+
+resource "aws_secretsmanager_secret" "hashed_credentials" {
+  name = "hashed-credentials-${random_string.random_secret_character.result}"
+}
+
+resource "aws_secretsmanager_secret_version" "hashed_credentials_version" {
+  secret_id = aws_secretsmanager_secret.hashed_credentials.id
+  secret_string = jsonencode({
+    "hash" : random_string.random_secret_hash.result
+  })
+}
+
+resource "random_string" "random_secret_character" {
+  length    = 5
+  special   = false
+  min_lower = 4
+}
+
+resource "random_string" "random_secret_hash" {
+  length  = 16
+  special = false
+  lower   = true
 }
