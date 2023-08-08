46400bd77daebb5a2800c0e4a351299dd2ed1ae8
kaushik.katariya <kaushik.katariya@it-objects.de>
2023-08-08 13:18:29 +0200

    chore: removed unnecessary parameters.

--- examples/example_2/main.tf
+++ examples/example_2/main.tf
@@ -6,43 +6,8 @@
 
 locals {
   solution_name = "terra3-example2"
-  aws_principals_account_ids_list = [
-    for account_id in var.access_for_account_ids : format("arn:aws:iam::%s:root", account_id)
-  ]
-}
-/*
-
-resource "aws_ecr_repository_policy" "ecr_repo_policy_xyz" {
-  count      = length(var.access_for_account_ids) > 1 ? 1 : 0
-
-  repository = "aws_ecr_repository.ecr_repo.name"
-  policy     = data.aws_iam_policy_document.ecr_repo_policy_xyz.json
-
 }
 
-
-data "aws_iam_policy_document" "ecr_repo_policy_xyz" {
-  statement {
-    sid    = "AllowCrossAccountPull"
-    effect = "Allow"
-
-    principals {
-      type        = "AWS"
-      identifiers = local.aws_principals_account_ids_list #["123456789012"]
-    }
-
-    actions = [
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:BatchGetImage",
-      "ecr:GetDownloadUrlForLayer"
-    ]
-  }
-}
-*/
-
-##
-# One logic is missing either with "id" or "ids", now both can be given.
-##
 module "terra3_examples" {
   source = "../.."
 
@@ -51,11 +16,6 @@ module "terra3_examples" {
 
   # configure your environment here
   create_load_balancer = true
-
-  create_ecr      = true
-  ecr_custom_name = "artproject-api"
-  #ecr_access_for_account_id = "433774759729"
-  ecr_access_for_account_ids = ["433774759729", "777458043667"]
 
   # dependency: required for downloading container images
   nat = "NAT_INSTANCES"
