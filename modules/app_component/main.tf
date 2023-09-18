2ddf4bf91a63385d0c97af83275ea7b05b379ba9
kaushik.katariya <kaushik.katariya@it-objects.de>
2023-09-18 16:11:27 +0200

    fix: added iam policy version for container-based cron job

--- modules/app_component/main.tf
+++ modules/app_component/main.tf
@@ -917,6 +917,7 @@ resource "aws_iam_role" "rule_role" {
   name = "${var.name}-rule-role"
 
   assume_role_policy = jsonencode({
+    Version = "2012-10-17"
     Statement = [
       {
         Action = "sts:AssumeRole",
